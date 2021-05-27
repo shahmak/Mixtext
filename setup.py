3886c201d3e5600cfb13678ee46ba4032cb3ce13
shahmak <84551015+shahmak@users.noreply.github.com>
2021-05-27 10:04:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup,find_packages
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -17,7 +17,7 @@ setup(
     license = "MIT",
     keywords = "Mixtext",
     url = "https://github.com/shahmak/Mixtext.git",
-    packages=['code'],
+    packages=find_packages(),
 #     long_description=read('README.md'),
     
 )
